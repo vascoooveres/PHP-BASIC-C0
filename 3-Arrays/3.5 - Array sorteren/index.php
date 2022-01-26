53f3e3a7b68986c0ff4b0d418d06f33eaba4de2a
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-01-26 11:44:45 +0100

    3,5

--- 3-Arrays/3.5 - Array sorteren/index.php
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -1,7 +1,9 @@
 <?php
-$getallen = ['676', '5345', '78657', '3', '46', '21', '47'];
-$laag_hoog = ['3', '21', '46', '47', '676', '5345', '78657'];
-print_r($laag_hoog);
-$hoog_laag = ['78657', '5345', '676', '47', '46', '21', '3'];
-print_r($hoog_laag);
+    $coders =  [676,5345,78657,3,46,21,47,];
+    asort ($coders);
+    print_r($coders);
+
+    $laag_hoog = [3,21,46,47,676,5345,78657];
+    arsort ($laag_hoog);
+    print_r($laag_hoog);
 ?>
