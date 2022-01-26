f7d3ab870325023561e9cbe02efa5e1a961b37b6
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-01-26 11:51:42 +0100

    ul verbeteren

--- 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -1,6 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $voornaam1 = "voornaam1";
 $voornaam2 = "voornaam2";
 $voornaam3 = "voornaam3";
-echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+echo "<ul> <li>$voornaam1</li> <li>$voornaam2</li> <li>$voornaam3</li> </ul>";
 ?>
+</body>
+</html>
